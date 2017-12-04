5c4d4edc9818fabaf34ad20ef8ba571ecc2ef014
Glitch (honey-form) <none>
2017-12-04 22:19:34 +0000

    🗃🐵 Checkpoint server.js:575125

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(function(err, req, res, next) {
 })
 
 app.listen(process.env.PORT, function () {
-  console.log('Node.js listening ...');
+  console.log('Node.js listening at port ' + process.env.PORT);
 });
 
 app.get('/', function(req, res){
