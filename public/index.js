a33d355c77502a6d2f4ab407c2f4fcd7a4fbf9d7
Glitch (honey-form) <none>
2017-12-05 22:09:44 +0000

    🐞⭐️ Checkpoint views/index.html:575125 public/index.js:575125

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $("#get-time").on('click', function(){
-    
-  });
+    var time = $("#raw-time").val().toString();
+    $.ajax("http://honey-form.glitch.me/time");
 });
