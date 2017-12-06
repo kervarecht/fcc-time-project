2f453f4989ef330e8ad2130dedca59c9a8e1fcb5
Glitch (honey-form) <none>
2017-12-06 22:20:01 +0000

    🌏🙅 Checkpoint time.js:575125

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var time = require("./time")
+var moment = require('moment');
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
@@ -42,7 +42,7 @@ app.route('/')
 
 app.route("/time")
   .get(function(req, res){
-  var time = req.query.data;
+  var rawTime = req.query.data;
   
   
 });
--- time.js
+++
@@ -1,8 +0,0 @@
-var moment = require("moment");
-
-var time = function(err, data){
-  if (err) throw err;
-  return moment(data).format();
-}
-
-module.exports;
